215fd9b7014457d0168a215091c6b4c286250cdc
Nguyễn Quang Anh <127297496+quanganh123@users.noreply.github.com>
2023-04-23 20:39:48 +0700

    Sua menu

--- ShowIOS.php
+++ ShowIOS.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width = device-width, initial-scale = 0.1">
-    <link rel="stylesheet" href="QLkhoCHDT.css">
+    <link rel="stylesheet" href="QLkhoCHDT1.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <title>Quản Lý Kho Cửa Hàng Điện Thoại</title>
 </head>
@@ -16,9 +16,9 @@
         <a class="logogit" href="https://github.com/quanganh123/quanlykhoCHDT/tree/master"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="50px"></a>
     </div>
     <div class="menu">
-        <li><a href="http://localhost/code/indexAdmin.php">Quản Lý Admin</a></li>
-        <li><a href="http://localhost/code/indexIos.php">Quản Lý Điện Thoại Ios</a></li>
-        <li><a href="http://localhost/code/indexAndroid.php">Quản Lý Điện Thoại Android</a></li>
+        <li><a href="http://localhost/code/indexAdmin.php"><img src="https://clipground.com/images/user-icon-vector-png-6.png" width="15px"> Quản Lý Admin</a></li>
+        <li><a href="http://localhost/code/indexIos.php"><img src="https://th.bing.com/th/id/R.312e81f96596fb37324b0188ca666ecf?rik=ZmjEfy8Zm2t2hA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_229805.png&ehk=87HeIWyeVqocnpW3QYc5PbCClLCVbOvudLl09z7%2fky4%3d&risl=&pid=ImgRaw&r=0" width="15px"> Quản Lý Điện Thoại Ios</a></li>
+        <li><a href="http://localhost/code/indexAndroid.php"><img src="https://1000marcas.net/wp-content/uploads/2020/01/Logo-Android-2048x1280.png" width="30px"> Quản Lý Điện Thoại Android</a></li>
     </div>
     <div class="container">
         <div class="namefrom">
