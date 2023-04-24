654698ffc44d5c31445c9e938a99d43d906f8712
Nguyễn Quang Anh <127297496+quanganh123@users.noreply.github.com>
2023-04-24 20:11:24 +0700

    Sửa logo

--- ShowANDROID.php
+++ ShowANDROID.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width = device-width, initial-scale = 0.1">
-    <link rel="stylesheet" href="QLkhoCHDT.css">
+    <link rel="stylesheet" href="QLkhoCHDT1.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <title>Quản Lý Kho Cửa Hàng Điện Thoại</title>
 </head>
