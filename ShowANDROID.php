c7a0a0d31e66619364a1860cae12c3bf73981dbb
Nguyễn Quang Anh <127297496+quanganh123@users.noreply.github.com>
2023-04-29 19:58:14 +0700

    Sửa Giao diện vs phân trang

--- ShowANDROID.php
+++ ShowANDROID.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width = device-width, initial-scale = 0.1">
-    <link rel="stylesheet" href="QLkhoCHDT2.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="QLkhoCHDT.css">
     <title>Quản Lý Kho Cửa Hàng Điện Thoại</title>
 </head>
-
 <body>
     <div class="logo">
         <img src="https://clipground.com/images/mobile-clipart-vector-9.jpg" width="50px">
         Quản Lý Kho Cửa Hàng Điện Thoại
-        <a class=" btnt btn btn-danger" onclick="return confirm('Bạn chắc chắn muốn thoát!');" href="/code/menungoai.php">Menu Ngoài</a></li>
+        <a class="btn" onclick="return confirm('Bạn chắc chắn muốn thoát!');" href="/code/menungoai.php">Menu Ngoài</a>
         <a class="logogit" href="https://github.com/quanganh123/quanlykhoCHDT/tree/master"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="50px"></a>
     </div>
     <div class="menu">
@@ -21,35 +18,31 @@
         <li><a href="http://localhost/code/indexIos.php"><img src="https://th.bing.com/th/id/R.312e81f96596fb37324b0188ca666ecf?rik=ZmjEfy8Zm2t2hA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_229805.png&ehk=87HeIWyeVqocnpW3QYc5PbCClLCVbOvudLl09z7%2fky4%3d&risl=&pid=ImgRaw&r=0" width="15px"> Quản Lý Điện Thoại Ios</a></li>
         <li><a href="http://localhost/code/indexAndroid.php"><img src="https://1000marcas.net/wp-content/uploads/2020/01/Logo-Android-2048x1280.png" width="30px"> Quản Lý Điện Thoại Android</a></li>
     </div>
-    <div class="container">
-        <div class="namefrom">
-            <h4>Quản Lý Điện Thoại Android</h4>
-        </div>
-        <table class="table table-striped table-hover table-sm text-center">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Tên Máy</th>
-                    <th>Cấu Hình</th>
-                    <th>Trạng Thái</th>
-                    <th>Số Lượng</th>
-                    <th></th>
-                    <th><a class="btn btn-info" href="FromAddmewAndroid.php">Thêm mới</a></th>
+    <div class="namefrom">
+        <h4>Quản Lý Điện Thoại Hãng Android</h4>
+    </div>
+    <div class="table1">
+        <table class="table">
+            <tr>
+                <th>Id</th>
+                <th>Tên Máy</th>
+                <th>Cấu Hình</th>
+                <th>Trạng Thái</th>
+                <th>Số Lượng</th>
+                <th></th>
+                <th><a class="btn3" href="FromAddmewAndroid.php">Thêm mới</a></th>
+            </tr>
+            <?php foreach ($ListAndroid as $Android) { ?>
+                <tr class="hover">
+                    <td><?php echo $Android["Id"] ?></td>
+                    <td><?php echo $Android["TenMay"] ?></td>
+                    <td><?php echo $Android["CauHinh"] ?></td>
+                    <td><?php echo $Android["TrangThai"] ?></td>
+                    <td><?php echo $Android["SoLuong"] ?></td>
+                    <td><a class="btn2" href="/code/indexAndroid.php?action=update&Id=<?php echo $Android["Id"] ?>">Sửa</a></td>
+                    <td><a class="btn4" onclick="return confirm('Bạn chắc chắn muốn thoát!');" href="/code/indexAndroid.php?action=delete&Id=<?php echo $Android["Id"] ?>">Xóa</a></td>
                 </tr>
-            </thead>
-            <tbody class="myTable">
-                <?php foreach ($ListAndroid as $Android) { ?>
-                    <tr>
-                        <td><?php echo $Android["Id"] ?></td>
-                        <td><?php echo $Android["TenMay"] ?></td>
-                        <td><?php echo $Android["CauHinh"] ?></td>
-                        <td><?php echo $Android["TrangThai"] ?></td>
-                        <td><?php echo $Android["SoLuong"] ?></td>
-                        <td><a class=" btnxs btn btn-success" href="/code/indexAndroid.php?action=update&Id=<?php echo $Android["Id"] ?>">Sửa</a></td>
-                        <td><a class=" btnxs btn btn-danger" onclick="return confirm('Bạn có muốn xóa dữ liệu không!');" href="/code/indexAndroid.php?action=delete&Id=<?php echo $Android["Id"] ?> ">Xóa</a></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
+            <?php } ?>
         </table>
         <?php
         include './sotrang2.php';
@@ -57,5 +50,4 @@
     </div>
     <div class="kengang"></div>
 </body>
-
 </html>
--- sotrang2.php
+++ sotrang2.php
@@ -1,17 +1,5 @@
 <ul class="pagination justify-content-end">
-<?php if ($limit > 1) { 
-        $prev_pare = $limit - 1 ?>
-        <li class="page-item"><a class="page-link" href="?per-page = <?= $limit ?> & page = <?= $prev_pare ?>">Đầu</a>
-        </li>
-    <?php } ?>
-    <?php for ($i = 1; $i <= $totalRecord / $limit; $i++) { ?>
-        <?php
-        $offset = ($i - 1) * $limit;
-        ?>
-        <li class="page-item"><a class="page-link" href="/code/indexAndroid.php?offset=<?php echo $offset ?>"><?php echo $i ?></a></li>
-    <?php } ?>
-    <?php if ($limit < $totalRecord - 1) {
-        $next_page = $totalRecord + 1 ?>
-        <li class="page-item"><a class="page-link" href="/code/indexAndroid.php?offset=<?php echo $offset ?>">Cuối</a></li>
+    <?php for ($i = 1; $i <= $total_pages; $i++) { ?>
+        <li><a class="btntrang" href="/code/indexAndroid.php?page=<?php echo $i ?>"><?php echo $i ?></a></li>
     <?php } ?>
 </ul>
